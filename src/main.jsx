b5d2192a6b40e86c848209c7f95a173cd7c500e1
Izaan Kamal <izaankml@gmail.com>
2026-08-18 14:48:07 -0700

    Home-screen app picks up new builds: check index.html on foreground and reload when the bundle changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "@fontsource-variable/nunito";
 import "./App.css";
+import { startUpdateChecks } from "./logic/updateCheck.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+startUpdateChecks();
